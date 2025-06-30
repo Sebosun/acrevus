7b81ff8262e58be16db36fc5abf55c894f9c2457
Sebastian <sebos@protonmail.com>
2025-06-30 23:16:30 +0200

    refactor: split update operation

--- repl/update.go
+++ repl/update.go
@@ -3,46 +3,19 @@ package repl
 import tea "github.com/charmbracelet/bubbletea"
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.sizes.width = msg.Width
-		m.sizes.height = msg.Height
-	case tea.KeyMsg:
-		// Cool, what was the actual key pressed?
-		switch msg.String() {
+	m, cmd := m.alwaysUpdate(msg)
+	if cmd != nil {
+		return m, cmd
+	}
 
-		// These keys should exit the program.
-		case "ctrl+c", "q":
-			return m, tea.Quit
+	m, cmd = m.updateReading(msg)
+	if cmd != nil {
+		return m, cmd
+	}
 
-		// The "up" and "k" keys move the cursor up
-		//
-		case "left", "h":
-			m.cursor = 0
-			m.selected = m.cursor
-			m.isReading = false
-
-		case "right", "l":
-			m.cursor = 0
-			m.selected = m.cursor
-			m.isReading = true
-
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-
-		// The "down" and "j" keys move the cursor down
-		case "down", "j":
-			if !m.isReading {
-				if m.cursor < len(m.articles)-1 {
-					m.cursor++
-				}
-			} else {
-				m.cursor++
-			}
-
-		}
+	m, cmd = m.updatePreview(msg)
+	if cmd != nil {
+		return m, cmd
 	}
 
 	// Return the updated model to the Bubble Tea runtime for processing.
@@ -50,5 +23,67 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m model) getArticleByIndex() {
+func (m model) alwaysUpdate(msg tea.Msg) (model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.sizes.width = msg.Width
+		m.sizes.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		// These keys should exit the program.
+		case "right", "l":
+			m.cursor = 0
+			m.selected = m.cursor
+			m.isReading = true
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			m.cursor++
+
+		}
+
+	}
+	return m, nil
+}
+
+func (m model) updatePreview(msg tea.Msg) (model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) updateReading(msg tea.Msg) (model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		case "left", "h":
+			m.cursor = 0
+			m.selected = m.cursor
+			m.isReading = false
+
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "j":
+			if m.cursor < len(m.articles)-1 {
+				m.cursor++
+			}
+
+		}
+	}
+
+	return m, nil
 }
